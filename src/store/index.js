5bd7f2ca47f1de42a93ff0709196ed5d4c63b88d
Abdullah Kocakaya <abdullahkocakaya@Abdullahs-iMac.local>
2020-05-17 16:29:29 +0300

    actions and mutations created and store modified

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'  //The vuex-persist package ensures that our Vuex state is saved between page reloads or refreshes.
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
 
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  mutations,
+  actions,
+  plugins: [vuexLocal.plugin],
+  strict: debug
 })
